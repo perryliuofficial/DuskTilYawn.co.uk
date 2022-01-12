904283cb7f8e19a254cc9317b8b880b135088375
Perry <59033672+perryliuofficial@users.noreply.github.com>
2022-01-12 15:19:07 +0000

    added more code comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,26 @@
-// marks items as red if their class is the same as current UK time (does not account for summer time)
+// marks items as red if their class is the same as current UK time (possibly does not account for summer time???)
+// yyyy MM dd hh mm
 function updatePage() {
     var today = new Date();
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     var yyyy = today.getFullYear();
-    var day = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var date = String(today.getDate()).padStart(2, '0');
     var minutes = today.getMinutes();
     var hour = today.getHours();
     if (minutes < 30) 
         minutes = "00";
     else minutes = 30;
-    var searchTime = [yyyy,mm,day,hour,minutes].join('');
+    var searchTime = [yyyy,mm,date,hour,minutes].join('');
     var x = document.getElementsByClassName(searchTime);
     var i;
     for (i = 0; i < x.length; i++) {
         x[i].style.color= "red";
     }
-    }
-    function startUpdate() 
-    {
-        updatePage();
-        window.setInterval(updatePage, 60 * 1000);
-    }
+}
+
+function startUpdate() {
+    updatePage();
+    window.setInterval(updatePage, 60 * 1000);
+}
+
 window.onload=startUpdate;
