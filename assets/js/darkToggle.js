97e0422564b2ba5459b444e03d54fcc4e713459a
Perry <59033672+perryliuofficial@users.noreply.github.com>
2022-01-15 17:54:37 +0000

    Update darkToggle.js

--- assets/js/darkToggle.js
+++ assets/js/darkToggle.js
@@ -1,27 +1,20 @@
 if (localStorage.getItem('theme') === null) {
   theme = "dark";
   localStorage.setItem('theme', theme);
-  console.log("no")
-  console.log(theme)
 }
 else {
   theme = localStorage.getItem('theme');
-  console.log("yes")
-  console.log(theme)
 }
 
 function changeColour() {
   if (theme == "dark") {
     document.documentElement.setAttribute('data-theme', 'dark');
-    console.log("adsdasasdasd")
   }
-  if (theme == "light"){
+  else {
     document.documentElement.setAttribute('data-theme', 'light');
-    console.log("adssadasdassadas")
   }
 }
 changeColour(theme);
-
 
 function darkToggle() {
   if (theme == "dark") {theme = "light"}
